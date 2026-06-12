044400048d63b64ea0940367433b8480ece2b38a
Diego <diegoaimargdlr@gmail.com>
2026-06-11 22:38:15 -0600

    Arreglando servidor de backend

--- Backend/demo-app/demo/src/main/java/com/example/demo/config/WebConfig.java
+++ Backend/demo-app/demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173\", \"https://diego-virtual-linux.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173\", \"https://diego-virtual-linux.vercel.app/")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
