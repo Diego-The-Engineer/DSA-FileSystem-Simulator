a86adb433f79450b0ec0157e69bc20279c546d63
Diego <diegoaimargdlr@gmail.com>
2026-06-11 22:50:04 -0600

    Version final

--- Frontend/virtual-terminal/src/App.jsx
+++ Frontend/virtual-terminal/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         return;
       }
       try {
-        const response = await fetch('https://virtualos-backend.onrender.com/', {
+        const response = await fetch('https://virtualos-backend.onrender.com/execute', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ command: commandToRun })
